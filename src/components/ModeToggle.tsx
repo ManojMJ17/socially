0645d6cdf3953100536adbc8c0008f38c4758673
Manoj Kumar <shinchanmj122@gmail.com>
2025-01-21 19:39:07 +0530

    Normalize line endings

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export default function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // Prevent rendering until the component is mounted
+
+  return (
+    <Button
+      variant={"outline"}
+      size={"icon"}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle Theme</span>
+    </Button>
+  );
+}
